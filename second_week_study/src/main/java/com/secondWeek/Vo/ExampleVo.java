fbe57e0728fc0a746a66e9bd17db86101ce404b2
YJunsu03 <YJunsu03@users.noreply.github.com>
2020-12-07 14:25:54 +0900

    add : Vo 생성, Controller 생성
    Vo 는 생성 완료
    Controller 는 아직 @관련 공부중

---
+++ second_week_study/src/main/java/com/secondWeek/Vo/ExampleVo.java
@@ -0,0 +1,13 @@
+package com.secondWeek.Vo;
+
+import lombok.Data;
+
+@Data
+
+public class ExampleVo {
+	private String userId;
+	private String userPw;
+	private String uesrName;
+	private String userBirthdate;
+	
+}
