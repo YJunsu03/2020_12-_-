8947cc8dc55862327ac80ea3594e7576d2a10f57
YJunsu03 <YJunsu03@users.noreply.github.com>
2020-12-10 10:35:38 +0900

    생성 : 실습 파일 1
    실습할 파일을 새로운 폴더와 파일로
    생성

---
+++ VS_Code/Learning/Example01_Function.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION function_name(param1 type, param2 type)
+ -- function_name 함수 이름
+ -- param1 type, param2 type 함수의 인자
+ -- 인자의 이름 param1, 2
+ -- type에 인자의 타입 쓰면됨
+ RETURNS return_type AS
+ -- return_type은 이 함수의 반환값의 타입 의미
+BEGIN
+ -- code(이곳에 함수가 실행할 코드 입력)
+END;
+LANGUAGE language_name;
