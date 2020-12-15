d4a65b2c7291e4a0626740b6af19c046460bd8a0
YJunsu03 <YJunsu03@users.noreply.github.com>
2020-12-15 17:00:21 +0900

    추가 : JAVA for문 기초예제 복습하다.
    백준 코딩 문제 사이트에 for문 관련
    기초 복습하다

---
+++ VS_Code/JAVA/for5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class for5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int sum=0;
+        for(int i=1; i<=input; i++){
+            System.out.println(i);
+        }
+    }
+}
