269649e782fde00bdff92f693104d6c8d3870b3b
YJunsu03 <YJunsu03@users.noreply.github.com>
2020-12-15 11:45:37 +0900

    추가 : 머릿속 첫 구구단 알맞은 코드 추가

--- VS_Code/JAVA/GUGUDAN.java
+++ VS_Code/JAVA/GUGUDAN.java
@@ -3,33 +3,108 @@ import java.util.Scanner;
 public class GUGUDAN {
     public static void main(String[] args) {
         int number=0;
+        String YesNo;
+        System.out.print("보고 싶은 구구단 숫자를 입력하세요 : ");
         Scanner sc = new Scanner(System.in);
         number = sc.nextInt();
-        switch(number) {
-            case 2 : 
+        while(true) {
+            if(number == 2) {
                 System.out.println("----2단----");
                 for(int i=2; i<=2; i++) {
                     for(int j=1; j<=9; j++) {
-                        System.out.print("|  " + i + "x" + j + "=" + i*j);
-
-                        if(i <= 4) {
-                        System.out.print("  |");
-                            if(i > 4) {
-                                System.out.println(" |");
-                            }
-                        }
+                        System.out.println(i + "x" + j + "=" + i*j);
                     }
                     System.out.println("");
                 }
-                System.out.println("______");
-            break;
-            case 3 : 
-                for(int i=3; i<=9; i++) {
-                    for(int j=1; j<i; j++) {
-                        System.out.println("\n");
+               
+            }
+
+            if(number == 3) {
+              System.out.println("----3단----");
+                for(int i=3; i<=3; i++) {
+                    for(int j=1; j<=9; j++) {
+                        System.out.println(i + "x" + j + "=" + i*j);
                     }
+                    System.out.println("");
                 }
-            break;
+            }
+
+            if(number == 4) {
+              System.out.println("----4단----");
+                for(int i=4; i<=4; i++) {
+                    for(int j=1; j<=9; j++) {
+                        System.out.println(i + "x" + j + "=" + i*j);
+                    }
+                    System.out.println("");
+                }
+            }
+
+            if(number == 5) {
+                System.out.println("----5단----");
+                  for(int i=5; i<=5; i++) {
+                      for(int j=1; j<=9; j++) {
+                          System.out.println(i + "x" + j + "=" + i*j);
+                      }
+                      System.out.println("");
+                  }
+            }
+
+              if(number == 6) {
+                System.out.println("----6단----");
+                  for(int i=6; i<=6; i++) {
+                      for(int j=1; j<=9; j++) {
+                          System.out.println(i + "x" + j + "=" + i*j);
+                      }
+                      System.out.println("");
+                  }
+            }
+
+              if(number == 7) {
+                System.out.println("----7단----");
+                  for(int i=7; i<=7; i++) {
+                      for(int j=1; j<=9; j++) {
+                          System.out.println(i + "x" + j + "=" + i*j);
+                      }
+                      System.out.println("");
+                  }
+            }
+
+              if(number == 8) {
+                System.out.println("----8단----");
+                  for(int i=8; i<=8; i++) {
+                      for(int j=1; j<=9; j++) {
+                          System.out.println(i + "x" + j + "=" + i*j);
+                      }
+                      System.out.println("");
+                  }
+            }
+
+              if(number == 9) {
+                System.out.println("----9단----");
+                  for(int i=9; i<=9; i++) {
+                      for(int j=1; j<=9; j++) {
+                          System.out.println(i + "x" + j + "=" + i*j);
+                      }
+                      System.out.println("");
+                  }
+            }
+            System.out.println("구구단 프로그램을 종료하시겠습니까?");
+            System.out.print("(Y/N)");
+            Scanner yn = new Scanner(System.in);
+            YesNo = yn.next();
+            if(YesNo.equals("Y")) {
+                break;
+            } else if(YesNo.equals("Yes")) {
+                break;                       
+            } else if(YesNo.equals("No")) {
+                continue;
+            } else if(YesNo.equals("N")) {
+                continue;
+            }
         }
+
+             
+        }   
+        
     }
-}
+
