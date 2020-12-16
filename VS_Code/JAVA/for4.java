f09a129cfcad59f985acae557d55a02b66d2c44c
YJunsu03 <YJunsu03@users.noreply.github.com>
2020-12-16 10:02:37 +0900

    수정 : Rename
    파일 이름 변경으로 인한 커밋

--- VS_Code/JAVA/for4.java
+++ VS_Code/JAVA/for4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class for5 {
+public class for4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
