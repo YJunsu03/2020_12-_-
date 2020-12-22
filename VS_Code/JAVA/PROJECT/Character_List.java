bb750d7202de306b59091a94a680f65060684899
YJunsu03 <YJunsu03@users.noreply.github.com>
2020-12-22 17:01:26 +0900

    생성 : pvp턴제 게임 프로젝트 파일

---
+++ VS_Code/JAVA/PROJECT/Character_List.java
@@ -0,0 +1,29 @@
+package PROJECT;
+
+public class Character_List {
+    public static void main(String[] args) {
+        // 캐릭터 리스트 Vo
+        String Yasuo = "Yasuo";
+        String Ahri = "Ahri";
+        String Garen = "Garen";
+        String Ani = "Ani";
+        String Evlin = "Evlin";
+        String Maokai = "Maokai";
+    }
+
+    public static int CharacterAttackStatus(int cas) {
+        int YasuoA = 50;
+        int AhriA = 80;
+        int GarenA = 30;
+        int AniA = 75;
+        int Evilin = 100;
+        int Maokai = 20;
+
+        return 1;
+    }
+
+}
+
+
+
+
---
+++ VS_Code/JAVA/PROJECT/Monster_List.java
@@ -0,0 +1,26 @@
+package PROJECT;
+
+public class Monster_List {
+    public static void main(String[] args) {
+        // 몬스터 리스트 Vo
+        String Slime = "Slime";
+        String Fox = "Fox";
+        String Jaejun = "Jaejun";
+        String Pig = "Pig";
+        String Goblin = "Goblin";
+        String Boss = "Boss :: Devil";
+        
+    }
+
+    public static String MonsterAttackStatus(int mas) {
+        int SlimeA = ;
+        int FoxA = ;
+        int JaejunA = ;
+        int PigA = ;
+        int GoblinA = ;
+        int BossA = ";
+
+        return;
+    }
+}
+
